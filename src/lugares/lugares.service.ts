16bcb0b41d4ee6812e9d2ae2cc3ad96648f1e1d8
Pedro Vitor Carvalho Ferreira da Silva <carvalho.ferreira@academico.ifrn.edu.br>
2024-09-07 20:55:31 -0300

    finalizando e corrigindo CRUD da tabela lugares

--- src/lugares/lugares.service.ts
+++ src/lugares/lugares.service.ts
@@ -1,83 +1,59 @@
 import { Injectable } from '@nestjs/common';
 import { CreateLugareDto } from './dto/create-lugare.dto';
 import { UpdateLugareDto } from './dto/update-lugare.dto';
-import { LugareEntity } from './entities/lugare.entity';
 import { PersistenciaService } from 'src/persistencia/persistencia.service';
+
+
 
 
 @Injectable()
 export class LugaresService {
   constructor(private persistencia: PersistenciaService) {}
 
-  lugares: LugareEntity[] = [];
-  contador: number = 0;
   async create(createLugareDto: CreateLugareDto) {
-    const l = await this.persistencia.lugare.create({data: createLugareDto})
-    // const lugare = {
-    //   id: this.contador + 1,
-    //   ...createLugareDto,
-    // };
-    // this.contador += 1;
-    // this.lugares.push(lugare);
+      return this.persistencia.lugare.create({
+        data: createLugareDto,
+      });
+  }
+
+  async findAll() {
+    const lugares = await this.persistencia.lugare.findMany();
     return {
-      estado: 'ok',
-      dados: l,
+      lugares
     };
   }
 
-  findAll() {
-    return {
-      estado: 'ok',
-      dados: this.lugares,
-    };
-  }
-
-  findOne(id: number) {
-    const temp = this.lugares.filter((lugare) => lugare.id === id);
-    if (temp[0]) {
+  async findOne(id: number) {
+    const lugare = await this.persistencia.lugare.findUnique({
+      where: { id },
+    });
+    if (lugare) {
       return {
         estado: 'ok',
-        dados: temp[0],
+        dados: lugare,
       };
     } else {
       return {
         estado: 'nok',
-        mensagem: `lugare com #${id} não existe!`,
+        mensagem: `Lugar com id #${id} não existe!`,
       };
     }
   }
 
-  update(id: number, updateLugareDto: UpdateLugareDto) {
-    const index = this.lugares.findIndex((lugare) => lugare.id === id);
-    if (index >= 0) {
-      this.lugares[index] = {
-        ...this.lugares[index],
-        ...updateLugareDto,
-      };
-      return {
-        estado: 'ok',
-        dados: this.lugares[index],
-      }
-    }
+  async update(id: number, updateLugareDto: UpdateLugareDto) {
+    return this.persistencia.lugare.update({
+      where: { id },
+      data: updateLugareDto,
+    });
+  }
+
+  async remove(id: number) {
+    const lugare = await this.persistencia.lugare.delete({
+      where: { id },
+    });
     return {
-      estado: 'nok',
-      mensagem: `lugare com #${id} não existe!`,
+      estado: 'ok',
+      dados: lugare,
     };
-  }
-
-  remove(id: number) {
-    const index = this.lugares.findIndex((lugare) => lugare.id === id);
-    if (index >= 0) {
-      const lugaresRemovidas = this.lugares.splice(index, 1);
-      return {
-        estado: 'ok',
-        dados: lugaresRemovidas[0],
-      };
-    } else {
-      return {
-        estado: 'nok',
-        mensagem: `lugare com #${id} não existe!`,
-      };
-    }
   }
 }
